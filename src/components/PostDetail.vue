94b73e5f3eb3b21adb889d188740f59258eddb89
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-05-02 19:19:05 +0900

    innitial commit

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Home
+          Welcome to PostDetail
         </h1>
 
         <p class="subheading font-weight-regular">
