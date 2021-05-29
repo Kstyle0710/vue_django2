95d919df3553335c2821b412bde2a833aefb5087
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-05-29 16:26:05 +0900

    making post details with v-card componants

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -1,94 +1,58 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" lg="10">
+          <h1>Post Title here</h1>
+          <p>2021-07-10, written by Kstyle</p>
+        </v-col>
+      </v-row>
+      <v-row align="start" justify="center">
+        <v-col cols="12" sm="8" lg="7">
+          <v-card class="pa-2" outlined tile>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <div>
+              <strong>Tags</strong>
+              <v-chip class="ma-2" outlined>
+                python
+              </v-chip>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to PostDetail
-        </h1>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="4" lg="3">
+          <v-card class="pa-2 mb-5" tile>
+            <p>prev post</p>
+            <h2>
+              Previous Title here
+            </h2>
+          </v-card>
+          <v-card class="pa-2 mb-5" tile>
+            <p>next post</p>
+            <h2>
+              Next Title here
+            </h2>
+          </v-card>
+          <v-card class="pa-2 mb-5" tile>
+            <h2>
+              Tag Cloud
+            </h2>
+            <v-chip class="ma-2" color="green" text-color="white">
+              <v-avatar left class="green darken-4">
+                1
+              </v-avatar>
+              python
+            </v-chip>
+            <v-chip class="ma-2" color="orange" text-color="white">
+              django
+              <v-icon right>
+                mdi-star
+              </v-icon>
+            </v-chip>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
