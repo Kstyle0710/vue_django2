ac8f9e19cd3ca2dda4428bc166b916b370680fd5
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-05-31 21:50:06 +0900

    axios tag cloud coding

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -9,10 +9,15 @@
     <v-row align="start" justify="center">
       <v-col cols="12" sm="8" lg="7">
         <v-card class="pa-2" outlined tile>
-          <p style="white-space: pre-wrap;"> {{ post.content }} </p>
+          <p style="white-space: pre-wrap;">{{ post.content }}</p>
           <div>
             <strong>Tags</strong>
-            <v-chip class="ma-2" outlined v-for="(tag, index) in post.tags" :key="index">
+            <v-chip
+              class="ma-2"
+              outlined
+              v-for="(tag, index) in post.tags"
+              :key="index"
+            >
               {{ tag }}
             </v-chip>
           </div>
@@ -21,27 +26,37 @@
       <v-col cols="12" sm="4" lg="3">
         <v-card class="pa-2 mb-5" tile>
           <p>prev post</p>
-          <h2 v-if="post.prev" @click="fetchPostDetail(post.prev.id)" class="my-hover"> {{ post.prev.title }} </h2>
+          <h2
+            v-if="post.prev"
+            @click="fetchPostDetail(post.prev.id)"
+            class="my-hover"
+          >
+            {{ post.prev.title }}
+          </h2>
         </v-card>
         <v-card class="pa-2 mb-5" tile>
           <p>next post</p>
-          <h2 v-if="post.next" @click="fetchPostDetail(post.next.id)" class="my-hover"> {{ post.next.title }} </h2>
+          <h2
+            v-if="post.next"
+            @click="fetchPostDetail(post.next.id)"
+            class="my-hover"
+          >
+            {{ post.next.title }}
+          </h2>
         </v-card>
         <v-card class="pa-2 mb-5" tile>
           <h2>
             Tag Cloud
           </h2>
-          <v-chip class="ma-2" color="green" text-color="white">
-            <v-avatar left class="green darken-4">
-              1
-            </v-avatar>
-            python
-          </v-chip>
-          <v-chip class="ma-2" color="orange" text-color="white">
-            django
-            <v-icon right>
-              mdi-star
-            </v-icon>
+          <v-chip
+            v-for="(tag, index) in tagCloud"
+            :key="index"
+            class="ma-2"
+            :color="tag.color"
+            text-color="white"
+          >
+            <v-avatar left :class="tag.color + ' green darken-4'">{{ tag.count }}</v-avatar>
+            {{ tag.name }}
           </v-chip>
         </v-card>
       </v-col>
@@ -50,41 +65,63 @@
 </template>
 
 <script>
-import axios from "axios";
-  export default {
-    name: 'HelloWorld',
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
 
-    data: () => ({
-      post: {},
-    }),
+  data: () => ({
+    post: {},
+    tagCloud: [],
+  }),
 
-    created() {
-      console.log("created()...");
-      const postId = 2;
-      this.fetchPostDetail(postId);
-    },
+  created() {
+    console.log('created()...')
+    const postId = 2
+    this.fetchPostDetail(postId)
+    this.fetchTagCloud()
+  },
 
-    methods: {
-      fetchPostDetail(postId) {
-        console.log("fetchPostDetail()...", postId);
-        axios.get(`/api/post/${postId}/`)     // ES6 template literal 사용
-        .then(res => {
-          console.log("POST DETAIL GET RES", res);
-          this.post = res.data;
+  methods: {
+    fetchPostDetail(postId) {
+      console.log('fetchPostDetail()...', postId)
+      axios
+        .get(`/api/post/${postId}/`) // ES6 template literal 사용
+        .then((res) => {
+          console.log('POST DETAIL GET RES', res)
+          this.post = res.data
         })
-        .catch(err => {
-          console.log("POST DETAIL GET ERR.RESPONSE", err.response);
-          alert(err.responsse.status + ' ' + err.response.statusText);
-        });
+        .catch((err) => {
+          console.log('POST DETAIL GET ERR.RESPONSE', err.response)
+          alert(err.responsse.status + ' ' + err.response.statusText)
+        })
+    },
+    fetchTagCloud() {
+      console.log('fetchTagCloud()...')
+      axios
+        .get('/api/tag/cloud/') // ES6 template literal 사용
+        .then((res) => {
+          console.log('TAG CLOUD GET RES', res)
+          this.tagCloud = res.data
+          // tag.weight
 
-      },
-    }
-  }
+          this.tagCloud.forEach(element => {
+            if (element.weight === 3) element.color = 'blue';
+            else if (element.weight === 2) element.color = 'brown';
+            else if (element.weight === 1) element.color = 'gray';
+          })
+        })
+        .catch((err) => {
+          console.log('TAG CLOUD GET ERR.RESPONSE', err.response)
+          alert(err.responsse.status + ' ' + err.response.statusText)
+        })
+    },
+  },
+}
 </script>
 
 <style scoped>
 .my-hover:hover {
   cursor: pointer;
   font-style: italic;
-}  
+}
 </style>
